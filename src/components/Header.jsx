df7111dc11ebaa2bc3148265d6e385fba03b0183
ankit9650 <ankitprabhaker9650@gmail.com>
2024-07-27 13:57:39 +0530

    Header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -40,6 +40,7 @@ function Header() {
             className="aspect-[3/2] object-cover lg:aspect-[4/3] lg:h-[700px] xl:aspect-[16/9] py-1 shadow-2xl"
             src="https://plus.unsplash.com/premium_photo-1678481760861-000b6f8904df?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
+            
           />
         </div>
       </div>
